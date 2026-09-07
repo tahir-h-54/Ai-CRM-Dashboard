12d54b2465b4f0be3af4e033cbf4c04ecce567b7
Tahir Hatim <tahirhatim54@gmail.com>
2026-09-07 23:59:46 +0500

    coded in authconetxt have some error

--- frontend/AI-CRM-Dashboard-UI/src/context/AuthContext.jsx
+++ frontend/AI-CRM-Dashboard-UI/src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {createContext, useContext, useState, useEffect, useCallback} from 'react';
 
+const AuthContext = createContext(null);
+
 const AuthContext = () => {
   return (
     <div>AuthContext</div>
