ef515408e9d461b8f5c4e1802982f86c49b9045f
Tahir Hatim <tahirhatim54@gmail.com>
2026-09-11 23:08:31 +0500

    Dashboard component is started but not complete

---
+++ frontend/AI-CRM-Dashboard-UI/src/components/dashboard/HeroCard.jsx
@@ -0,0 +1,11 @@
+import {Wifi} from 'lucide-react';
+import {Card, SectionHeading} from '../ui';
+import { currency } from '../../lib/format';
+
+export function HeroCard({ value = 0, label = "Pipline value "}) {
+    return(
+        <Card className='' >
+
+        </Card>
+    )
+}
