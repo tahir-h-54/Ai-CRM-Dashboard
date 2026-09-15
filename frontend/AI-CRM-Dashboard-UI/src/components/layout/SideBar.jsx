27b255c481b19f658d500b1921a96bb55dc02b80
Tahir Hatim <tahirhatim54@gmail.com>
2026-09-15 22:31:41 +0500

    imports added in sidebar

---
+++ frontend/AI-CRM-Dashboard-UI/src/components/layout/SideBar.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+import {
+  LayoutGrid,
+  Users,
+  Contact2,
+  KanbanSquare,
+  StickyNote,
+  CalendarCheck,
+  Settings,
+  LogOut,
+  Sparkles,
+} from "lucide-react";
